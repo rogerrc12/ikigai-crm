06311208fa5ecd0f4f0386e4bb3d2a4d485c8a41
Roger Rengifo <rogerrengifo@Rogers-MacBook-Pro.local>
2020-11-29 17:11:42 -0500

    fixing contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,7 +42,7 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: `qmm7610uwpqq`,
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
         // Learn about environment variables: https://gatsby.dev/env-vars
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
